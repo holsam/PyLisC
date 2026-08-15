76837083be622c4d2e07a29b6234f6d84fc97334
Sam Holland <hol_sam@icloud.com>
2026-08-15 20:54:00 +0100

    feat(lisc): make pixel size optional
    - Modified lisc.py to allow optional pixel size argument, and update
    logic to raise an error if apply_filter is set and no pixel size is
    given.

--- src/pylisc/lisc.py
+++ src/pylisc/lisc.py
@@ -5,6 +5,7 @@ PyLisC: apply filtering and destriping to a single 2D projection
 # Import external libraries
 import numpy as np
 from scipy import ndimage as ndi
+from typing import Optional
 
 # Import internal PyLisC libraries
 from pylisc.blur import bandpass_highpass
@@ -13,7 +14,7 @@ from pylisc.destripe import directional_destripe_angular, directional_destripe_l
 def lisc_clear_frame(
     frame: np.ndarray,
     decurtaining_mode: str,
-    pixel_size_nm: float,
+    pixel_size_nm: Optional[float] = None,
     curtain_angle: float = 0.0,
     apply_filter: bool = False,
     filter_threshold_nm: float = 5000.0,
@@ -28,6 +29,8 @@ def lisc_clear_frame(
 
     # Apply high-pass filter if specified
     if apply_filter:
+        if pixel_size_nm is None:
+            raise ValueError('pixel_size_nm is required when apply_filter is set')
         filter_px = max(filter_threshold_nm / pixel_size_nm, 1.0)
         frame = bandpass_highpass(frame, filter_px)
     
