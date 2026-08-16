a0a103c2e48d5b291f9b1b901c8d53244fda6a77
Sam Holland <hol_sam@icloud.com>
2026-08-16 10:32:26 +0100

    feat(cli): add -h option for help text
    - Modified cli.py to allow help text to be called using `-h` as well as
    `--help`.

--- src/pylisc/cli.py
+++ src/pylisc/cli.py
@@ -21,6 +21,7 @@ pylisc = typer.Typer(
     rich_markup_mode='rich',
     add_completion=False,
     no_args_is_help=True,
+    context_settings={'help_option_names': ['-h', '--help']},
 )
 
 # Shared options
